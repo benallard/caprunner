330a79a5c6becd42970316ea63d59d7ef567587e
Benoît Allard <benoit.allard@gmx.de>
2011-03-24 10:38:26 +0100

    Move utility functions to a utils module

---
+++ utils.py
@@ -0,0 +1,59 @@
+import struct # for general decoding of class files
+
+# Utility functions.
+
+def u1(data):
+    return struct.unpack(">B", data[0:1])[0]
+
+def u2(data):
+    return struct.unpack(">H", data[0:2])[0]
+
+def u4(data):
+    return struct.unpack(">L", data[0:4])[0]
+
+def s2(data):
+    return struct.unpack(">h", data[0:2])[0]
+
+def s4(data):
+    return struct.unpack(">l", data[0:4])[0]
+
+def s8(data):
+    return struct.unpack(">q", data[0:8])[0]
+
+def f4(data):
+    return struct.unpack(">f", data[0:4])[0]
+
+def f8(data):
+    return struct.unpack(">d", data[0:8])[0]
+
+## {{{ http://code.activestate.com/recipes/576563/ (r1)
+def cached_property(f):
+    """returns a cached property that is calculated by function f"""
+    def get(self):
+        try:
+            return self._property_cache[f]
+        except AttributeError:
+            self._property_cache = {}
+            x = self._property_cache[f] = f(self)
+            return x
+        except KeyError:
+            x = self._property_cache[f] = f(self)
+            return x
+        
+    return property(get)
+## end of http://code.activestate.com/recipes/576563/ }}}
+
+def u1a(size, data):
+    """ u1 array """
+    return [u1(data[i:i+1]) for i in xrange(size)]
+
+def u2a(size, data):
+    """ u2 array """
+    return [u2(data[i*2:i*2+2]) for i in xrange(size)]
+
+def a2s(a):
+    """ array to string """
+    return ' '.join(["%02X" % i for i in a])
+
+def stringify(s):
+    return ' '.join(["%02X" % ord(c) for c in s])
