f9628cb8391846d3ce4372e9aee7e4ac77f5606c
Benoît Allard <benoit.allard@gmx.de>
2011-04-06 23:36:48 +0200

    rslvr: a bit of more progress

--- resolver.py
+++ resolver.py
@@ -3,6 +3,9 @@ import pickle
 from utils import a2d
 
 class linkResolver(object):
+    """
+    We don't take care of version of packages ...
+    """
     def __init__(self, version=(3,0,1)):
         self.version = version
         # load preprocessed pickle from all the exp files
@@ -15,9 +18,9 @@ class linkResolver(object):
         """
         self.refs[a2d(exp.AID)] = exp.collectrefs()
 
-    def resolvePackage(self, aid):
-        """ return a python package corresponding to the aid """
-        pass
+    def hasPackage(self, aid):
+        """ return if a python package corresponding to the aid is known """
+        return aid in self.refs
 
     def resolveClass(self, aid, token):
         """ return a python class corresponding to the token """
@@ -36,3 +39,6 @@ if __name__ == "__main__":
     from exportfile import ExportFile
     rslvr = linkResolver()
     rslvr.addExportFile(ExportFile(open(sys.argv[1]).read()))
+    print rslvr.hasPackage('\xa0\x00\x00\x00b\x00\x01')
+    print rslvr.hasPackage('\xa0')
+
