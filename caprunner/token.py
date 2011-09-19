cf5ecd3a37d14d5beb2cfa19df8735e5528d1aad
Benoît Allard <benoit.allard@gmx.de>
2011-09-19 16:54:22 +0200

    Add a token module that does a lot that was done in runcap.py

---
+++ caprunner/token.py
@@ -0,0 +1,236 @@
+"""
+This is a Python token, it reprsents a Token + the CardManager
+
+"""
+
+from pythoncard.framework import Applet, ISO7816, ISOException, APDU, JCSystem, AID
+
+from caprunner import resolver, capfile
+from caprunner.utils import d2a, a2d
+from caprunner.interpreter import JavaCardVM, ExecutionDone
+from caprunner.interpreter.methods import JavaCardStaticMethod, JavaCardVirtualMethod, NoSuchMethod
+
+class Token(object):
+    """
+    This token is a Java Token. 
+    The Applet is actually a capfile. The code below is taken from runcap.py
+    """
+    def __init__(self, jcversion = (3,0,1)):
+
+        self.current_applet_aid = None
+        # a2d(aid) => Applet
+        self.applets = {}
+        # channel => Applet
+        self.selected = [None, None, None, None]
+        # opened
+        self.channels = [True, False, False, False]
+        # current one
+        self.current_channel = 0
+
+        # Create the VM
+        self.vm = JavaCardVM(resolver.linkResolver(jcversion))
+
+        self.installJCFunctions()    
+
+    def installJCFunctions(self):
+        """ This tweak the JC Framework to make it fit our environment 
+        We make a big usage of closures to pass the local `self` to the JC
+        functions
+        """
+
+        def defineMyregister(self):
+            def myregister(applet, bArray = [], bOffset=0, bLength=0):
+                if bLength != 0:
+                    aid = bArray[bOffset:bOffset + bLength]
+                    # also update the current one
+                    self.current_applet_aid = aid
+                else:
+                    aid = self.current_applet_aid
+                self.applets[a2d(aid)] = applet
+            return myregister
+        Applet.register = defineMyregister(self)
+
+        def defineMygetAID(self):
+            def mygetAID():
+                return AID(self.current_applet_aid, 0, len(self.current_applet_aid))
+            return mygetAID
+        JCSystem.getAID = defineMygetAID(self)
+        Applet.getAID = JCSystem.getAID
+
+        def defineMylookupAID(self):
+            def mylookupAID(buffer, offset, length):
+                if a2d(buffer[offset:offset + length]) in self.applets:
+                    return AID(buffer, offset, length)
+                return None
+            return mylookupAID
+        JCSystem.lookupAID = defineMylookupAID(self)
+
+        def defineMyisAppletActive(self):
+            def myisAppletActive(aid):
+                return applets[a2d(aid.aid)] in self.selected
+            return myisAppletActive
+        JCSystem.isAppletActive = defineMyisAppletActive(self)
+
+        def defineMygetAssignedChannel(self):
+            def mygetAssignedChannel():
+                return self.current_channel
+        JCSystem.getAssignedChannel = defineMygetAssignedChannel(self)
+
+    def transmit(self, bytes):
+        self.vm.log = ""
+        self.current_channel = bytes[0] & 0x3
+        if self.selected[self.current_channel]:
+            self.selected[self.current_channel]._selectingApplet = False
+        if not bool(bytes[0] & 0x80):
+            # ISO command
+            if bytes[1:4] == [-92, 4, 0]:
+                aid = bytes[5:5 + bytes[4]]
+                # select command A4 04 00
+                if not self._cmselect(aid):
+                    return d2a('\x69\x99')
+            elif bytes[1:4] == [112, 0, 0]:
+                # open channel : 70 00 00
+                for idx in xrange(4):
+                    if not self.channels[idx]:
+                        self.channels[idx] = True
+                        buf = [idx]
+                        buf.extend(d2a('\x90\x00'))
+                        return buf
+                return d2a(ISO7816.SW_WRONG_P1P2)
+            elif bytes[1:3] == [112, -128]:
+                # close channel: 70 80
+                idx = bytes[3]
+                if self.channels[idx]:
+                    self.channels[idx] = False
+                    return d2a('\x90\x00')
+                return d2a(ISO7816.SW_WRONG_P1P2)
+            elif bytes[1:4] == [-26, 12, 0]:
+                # install : E6 0C 00
+                self.install(bytes, 5)
+
+        applet = self.selected[self.current_channel]
+        if applet is None:
+            # no applet selected on current channel
+            return d2a(ISO7816.SW_FILE_NOT_FOUND)
+        # Make an APDU object
+        apdu = APDU(bytes)
+        # pass to the process method
+        self.vm.frame.push(applet)
+        self.vm.frame.push(apdu)
+        # invoke the process method
+        self.vm._invokevirtualjava(JavaCardVirtualMethod(
+                applet._ref.offset,
+                7, # process
+                False,
+                self.vm.cap_file,
+                self.vm.resolver))
+        try:
+            while True:
+                self.vm.step()
+        except ExecutionDone:
+            pass
+        except ISOException, isoe:
+            return d2a(isoe.getReason())
+        except Exception, e:
+            return d2a(ISO7816.SW_UNKNOWN)
+        buf = apdu._APDU__buffer[:apdu._outgoinglength]
+        buf.extend(d2a('\x90\x00'))
+        return buf
+
+    def _cmdeselect(self):
+        channel = self.current_channel
+        applet = self.selected[channel]
+        self.vm.frame.push(applet)
+        try:
+            deselectmtd = JavaCardVirtualMethod(applet._ref.offset, 4, False, vm.cap_file, vm.resolver)
+        except NoSuchMethod:
+            self.selected[channel] = None
+            return True
+        self.vm._invokevirtualjava(deselectmtd)
+        try:
+            while True:
+                vm.step()
+        except ExecutionDone:
+            pass
+        if vm.frame.getValue():
+            self.selected[channel] = None
+            return True
+        return False
+
+    def _cmselect(self, aid):
+        channel = self.current_channel
+       
+        applet = self.selected[channel]
+        if applet is not None:
+            if not self._cmdeselect():
+                return False
+
+        # We should spend some time looking for an applet there ...
+        try:
+            potential = self.applets[a2d(aid)]
+        except KeyError:
+            return False
+
+        self.vm.frame.push(potential)
+        try:
+            selectmtd = JavaCardVirtualMethod(
+                potential._ref.offset,
+                6,
+                False,
+                self.vm.cap_file,
+                self.vm.resolver)
+        except NoSuchMethod:
+            selected[channel] = potential
+            selected[channel]._selectingApplet = True
+            return True
+        self.vm._invokevirtualjava(selectmtd)
+        try:
+            while True:
+                self.vm.step()
+        except ExecutionDone:
+            pass
+        if self.vm.frame.getValue() == True:
+            self.selected[channel] = potential
+            self.selected[channel]._selectingApplet = True
+            return True
+        else:
+            return False
+
+    def install(self, data, offset):
+        """ 
+        data[offset:] is len||appletaid||len||installdata
+        where installdata is the data given to the install method
+        """
+        aidlen = data[offset]
+        offset += 1
+        aid = data[offset: offset + aidlen]
+        offset += aidlen
+        length = data[offset]
+        offset += 1
+        # data[offset:offset+length] is what is given to the install JavaCard 
+        # method which means: len-instanceaid-len-stuff-len-customparams
+        # where instance AID might be empty
+        self.vm.frame.push(data)
+        self.vm.frame.push(offset)
+        self.vm.frame.push(length)
+        applet = None
+        for apl in self.vm.cap_file.Applet.applets:
+            if a2d(aid) == a2d(apl.aid):
+                applet = apl
+            break
+        if applet is None:
+            return
+        self.current_applet_aid = aid
+        self.vm._invokestaticjava(JavaCardStaticMethod(
+                applet.install_method_offset,
+                self.vm.cap_file,
+                self.vm.resolver))
+        try:
+            while True:
+                self.vm.step()
+        except ISOException, ie:
+            return d2a(ie.getReason())
+        except ExecutionDone:
+            pass
+        self.current_install_aid = None
+        return d2a('\x90\x00')
--- runcap.py
+++ runcap.py
@@ -2,241 +2,53 @@
 
 import sys
 
-from caprunner import resolver, capfile
-from caprunner.utils import s2a, a2s, d2a, a2d, signed1
-from caprunner.interpreter import JavaCardVM, ExecutionDone
-from caprunner.interpreter.methods import JavaCardStaticMethod, JavaCardVirtualMethod, NoSuchMethod
+from caprunner import capfile
+from caprunner.utils import s2a, a2s
 
-from pythoncard.framework import Applet, APDU, ISOException, JCSystem, AID
+from caprunner.token import Token
 
-from python.lang import RuntimeException
+class Runner(Token):
 
-current_install_aid = None
-# a2d(aid) => Applet
-applets = {}
-# channel => Applet
-selected = [None, None, None, None]
-# opened
-channels = [True, False, False, False]
-# current one
-current_channel = 0
-
-def myregister(applet, *args):
-    if len(args) == 0:
-        applets[a2d(current_install_aid)] = applet
-        print "Registered %s as %s" % (applet, a2s(current_install_aid))
-    else:
-        (array, offset, length) = args
-        applets[a2d(array[offset:offset+length])] = applet
-        print "Registered %s as %s" %(applet, a2s(array[offset:offset+length]))
-
-Applet.register = myregister
-
-def mylookupAID(buffer, offset, length):
-    if a2d(buffer[offset:offset + length]) in applets:
-        return AID(buffer, offset, length)
-    return None
-
-JCSystem.lookupAID = mylookupAID
-
-def myisAppletActive(aid):
-    return applets[a2d(aid.aid)] in selected
-
-JCSystem.isAppletActive = myisAppletActive
-
-def mygetAssignedChannel():
-    return current_channel
-    
-JCSystem.getAssignedChannel = mygetAssignedChannel
-
-def _checkreceive(vm, expected, received):
-    print "<== %s" % a2s(received)
-    if expected != received:
-        print "<== %s was expected" % a2s(expected)
-        print vm.log
-        sys.exit()    
-
-def process(vm, send, receive):
-    print "==> %s" % a2s(send)
-    vm.log = ""
-    global current_channel
-    current_channel = send[0] & 0x3
-    if selected[current_channel]:
-        selected[current_channel]._selectingApplet = False
-    if not bool(send[0] & 0x80):
-        # ISO command
-        if send[1:4] == [-92, 4, 0]:
-            aid = send[5:5 + send[4]]
-            # select command
-            if not select(vm, current_channel, aid):
-                # Applet selection failed
-                _checkreceive(vm, d2a('\x69\x99'), receive)
-                return
-        elif send[1:4] == [112, 0, 0]:
-            # open channel
-            for idx in xrange(4):
-                if not channels[idx]:
-                    channels[idx] = True
-                    buf = [idx]
-                    buf.extend(d2a('\x90\x00'))
-                    _checkreceive(vm, buf, receive)
-                    return
-            print "No more channels"
+    def checkreceive(self, expected, received):
+        print "<== %s" % a2s(received)
+        if expected != received:
+            print "<== %s was expected" % a2s(expected)
+            print self.vm.log
             sys.exit()
-        elif send[1:3] == [112, -128]:
-            # close channel
-            idx = send[3]
-            if channels[idx]:
-                channels[idx] = False
-                _checkreceive(vm, d2a('\x90\x00'), receive)
-                return
+
+    def run(self):
+        send = []
+        receive = []
+        for line in sys.stdin:
+            line = line.rstrip()
+            if len(line) == 0 and len(send) != 0 and len(receive) != 0:
+                print "==> %s" % a2s(send)
+                got = self.transmit(send)
+                self.checkreceive(receive, got)
+                send = []
+                receive = []
+            if line[:4] == "==> ":
+                send.extend(s2a(line[4:]))
+            elif line[:4] == "<== ":
+                receive.extend(s2a(line[4:]))
+            elif line.startswith('install:'):
+                sep1 = 8
+                sep2 = line.find(':', sep1+1)
+                data = s2a(line[sep1:sep2])
+                offset = s2a(line[sep2 + 1:])[0]
+                self.install(data, offset)
+            elif line.startswith('load:'):
+                self.vm.load(capfile.CAPFile(line[5:].strip()))
+            elif line.startswith('log;'):
+                print self.vm.log
             else:
-                print "Channel %d not opened" % send[3]
-                sys.exit()
-    applet = selected[current_channel]
-    if applet is None:
-        # No applet selected on current channel.
-        # We don't understand the current INS
-        _checkreceive(vm, d2a('\x6D\x00'), receive)
-        return
+                print line
 
-    # Make an APDU object
-    apdu = APDU(send)
-    # pass to the process method
-    vm.frame.push(applet)
-    vm.frame.push(apdu)
-    # invoke the process method
-    vm._invokevirtualjava(JavaCardVirtualMethod(applet._ref.offset, 7, False, vm.cap_file, vm.resolver))
-    isoE = False
-    try:
-        while True:
-            vm.step()
-    except ExecutionDone:
-        pass
-    except ISOException, ie:
-        sw = ie.getReason()
-        buf = [signed1((sw & 0xff00) >> 8), signed1(sw & 0x00ff)]
-        isoE = True
-    except RuntimeException, rte:
-        print "processing throwed an exception of type %s" % type(rte)
-        buf = d2a('\x6f\x00')
-        isoE = True
-    if not isoE:
-        buf = apdu._APDU__buffer[:apdu._outgoinglength]
-        buf.extend(d2a('\x90\x00'))
-    _checkreceive(vm, receive, buf)
+if len(sys.argv) > 1:
+    version = tuple([int(i) for i in sys.argv[1].split('.')])
+    print "Running in version %s.%s.%s" % version
+    runner = Runner(version)
+else:
+    runner = Runner()
 
-def deselect(vm, channel):
-    applet = selected[channel]
-    vm.frame.push(applet)
-    vm._invokevirtualjava(JavaCardVirtualMethod(applet._ref.offset, 4, False, vm.cap_file, vm.resolver))
-    try:
-        while True:
-            vm.step()
-    except ExecutionDone:
-        pass
-    return vm.frame.getValue()
-
-def select(vm, channel, aid):
-    applet = selected[channel]
-    if applet is not None:
-        if not deselect(vm, channel):
-            print "Cannot deselect previous applet"
-            return False
-    try:
-        potential = applets[a2d(aid)]
-    except KeyError:
-        print "Applet with aid %s not found" % a2s(aid)
-        return False
-    vm.frame.push(potential)
-    try:
-        selectmtd = JavaCardVirtualMethod(potential._ref.offset, 6, False, vm.cap_file, vm.resolver)
-    except NoSuchMethod:
-        selected[channel] = potential
-        selected[channel]._selectingApplet = True
-        return True
-    vm._invokevirtualjava(selectmtd)
-    try:
-        while True:
-            vm.step()
-    except ExecutionDone:
-        pass
-    if vm.frame.getValue() == True:
-        selected[channel] = potential
-        selected[channel]._selectingApplet = True
-        return True
-    else:
-        print "Select failed"
-        return False
-
-def install(vm, data, offset):
-    global current_install_aid
-    aidlen = data[offset]
-    offset += 1
-    aid = data[offset: offset + aidlen]
-    offset += aidlen
-    length = data[offset]
-    offset += 1
-    vm.frame.push(data)
-    vm.frame.push(offset)
-    vm.frame.push(length)
-    applet = None
-    for apl in vm.cap_file.Applet.applets:
-        if a2d(aid) == a2d(apl.aid):
-            applet = apl
-            break
-    if applet is None:
-        print "No Applet installed ; %s not found" % a2s(aid)
-        return
-    current_install_aid = aid
-    vm._invokestaticjava(JavaCardStaticMethod(applet.install_method_offset, vm.cap_file, vm.resolver))
-    try:
-        while True:
-            vm.step()
-    except ISOException, ie:
-        sw = ie.getReason()
-        print "instanciating throwed : %02X %02X" % ((sw & 0xff00) >> 8, sw & 0x00ff)
-        print vm.log
-    except RuntimeException, rte:
-        print "instanciating throwed an exception of type %s" % type(rte)
-        print vm.log
-    except ExecutionDone:
-        pass
-    current_install_aid = None
-
-def main(jcversion = (3,0,1)):
-    vm = JavaCardVM(resolver.linkResolver(jcversion))
-
-    send = []
-    receive = []
-    for line in sys.stdin:
-        line = line.rstrip()
-        if len(line) == 0 and len(send) != 0 and len(receive) != 0:
-            process(vm, send, receive)
-            send = []
-            receive = []
-        if line[:4] == "==> ":
-            send.extend(s2a(line[4:]))
-        elif line[:4] == "<== ":
-            receive.extend(s2a(line[4:]))
-        elif line.startswith('install:'):
-            sep1 = 8
-            sep2 = line.find(':', sep1+1)
-            data = s2a(line[sep1:sep2])
-            offset = s2a(line[sep2 + 1:])[0]
-            install(vm, data, offset)
-        elif line.startswith('load:'):
-            vm.load(capfile.CAPFile(line[5:].strip()))
-        elif line.startswith('log;'):
-            print vm.log
-        else:
-            print line
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) > 1:
-        version = tuple([int(i) for i in sys.argv[1].split('.')])
-        print "Running in version %s.%s.%s" % version
-        main(version)
-    else:
-        main()
+runner.run()
