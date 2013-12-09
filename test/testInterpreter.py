ba448fbf6993911c4e4a73844e8d6d1fbcc11dcc
Benoît Allard <benoit.allard@gmx.de>
2013-12-09 22:20:56 +0100

    test: The JavaCardFrame object now needs an offset

--- test/testInterpreter.py
+++ test/testInterpreter.py
@@ -9,7 +9,7 @@ from pythoncard.framework import ISOException, Applet
 
 class TestDummyFrame(JavaCardFrame, DummyFrame):
     def __init__(self, params):
-        JavaCardFrame.__init__(self, params, [])
+        JavaCardFrame.__init__(self, params, [], 0)
     push = JavaCardFrame.push
     pop = JavaCardFrame.pop
 
@@ -31,27 +31,27 @@ class TestInterpreter(unittest.TestCase):
 
     def testSomeIf(self):
         intr = JavaCardVM(None)
-        intr.frames.push(JavaCardFrame([None, 6], [29, 5, 73, 97, 6, 29, 6, 69, 120, 3, 49, 30, 29, 109, 13, 30, 8, 65, 48, 30, 4, 65, 91, 49, 112, 243, 29, 120]))
+        intr.frames.push(JavaCardFrame([None, 6], [29, 5, 73, 97, 6, 29, 6, 69, 120, 3, 49, 30, 29, 109, 13, 30, 8, 65, 48, 30, 4, 65, 91, 49, 112, 243, 29, 120], 0))
         self._run(intr)
 
     def test_gcdRecursif(self):
         intr = JavaCardVM(linkResolver())
         intr.load(javatest_cap)
-        intr.frames.push(JavaCardFrame([42,56], [29, 97, 4, 28, 120, 29, 28, 29, 73, 141, 0, 9, 120]))
+        intr.frames.push(JavaCardFrame([42,56], [29, 97, 4, 28, 120, 29, 28, 29, 73, 141, 0, 9, 120], 0))
         self._run(intr)
         self.assertEquals(14, intr.getRetValue())
         
 
     def test_gcdIteratif(self):
         intr = JavaCardVM(None)
-        intr.frames.push(JavaCardFrame([42,56], [29, 97, 4, 28, 120, 28, 29, 73, 49, 29, 47, 30, 48, 112, 243]))
+        intr.frames.push(JavaCardFrame([42,56], [29, 97, 4, 28, 120, 28, 29, 73, 49, 29, 47, 30, 48, 112, 243], 0))
         self._run(intr)
         self.assertEquals(14, intr.getRetValue())
 
     def test_callExtStaticMethod(self):
         intr = JavaCardVM(linkResolver())
         intr.load(javatest_cap)
-        intr.frames.push(JavaCardFrame([None, None], [17, 106, 129, 141, 0, 5, 122]))
+        intr.frames.push(JavaCardFrame([None, None], [17, 106, 129, 141, 0, 5, 122], 0))
         try:
             self._run(intr)
             self.fail()
