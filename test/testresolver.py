817ef3502a5d7f7371ae9f1b02e7db7a1349a57a
Benoît Allard <benoit.allard@gmx.de>
2021-06-16 19:14:38 +0200

    More py3 fixing of the tests

--- test/testresolver.py
+++ test/testresolver.py
@@ -10,13 +10,13 @@ class TestResolver(unittest.TestCase):
     
     def test_basicPackage(self):
         rslvr = linkResolver()
-        self.assertTrue(rslvr.hasPackage('\xa0\x00\x00\x00b\x00\x01'))
-        self.assertFalse(rslvr.hasPackage('\xa0'))
+        self.assertTrue(rslvr.hasPackage(b'\xa0\x00\x00\x00b\x00\x01'))
+        self.assertFalse(rslvr.hasPackage(b'\xa0'))
 
     def test__resolveExtStaticMethod(self):
         rslvr = linkResolver()
         # ISOExcption.throwIt
-        mtd = rslvr._resolveExtStaticMethod('\xa0\x00\x00\x00b\x01\x01', 7, 1)
+        mtd = rslvr._resolveExtStaticMethod(b'\xa0\x00\x00\x00b\x01\x01', 7, 1)
         try:
             mtd(0x9000)
             self.fail()
@@ -37,13 +37,13 @@ class TestResolver(unittest.TestCase):
 
     def test__resolveExtClass(self):
         rslvr = linkResolver()
-        self.assertEqual(ISOException, rslvr._resolveExtClass('\xa0\x00\x00\x00b\x01\x01', 7).cls)
+        self.assertEqual(ISOException, rslvr._resolveExtClass(b'\xa0\x00\x00\x00b\x01\x01', 7).cls)
 
     def test_addExportFile(self):
         rslvr = linkResolver()
-        self.assertFalse(rslvr.hasPackage('\xA0\x00\x00\x00\x18\xFF\x00\x00\x00\x00\x00\x00\x00\x00\x01\x01'))
+        self.assertFalse(rslvr.hasPackage(b'\xA0\x00\x00\x00\x18\xFF\x00\x00\x00\x00\x00\x00\x00\x00\x01\x01'))
         rslvr.addExportFile(javatest_exp)
-        self.assertTrue(rslvr.hasPackage('\xA0\x00\x00\x00\x18\xFF\x00\x00\x00\x00\x00\x00\x00\x00\x01\x01'))
+        self.assertTrue(rslvr.hasPackage(b'\xA0\x00\x00\x00\x18\xFF\x00\x00\x00\x00\x00\x00\x00\x00\x01\x01'))
 
     def test_getModule(self):
         rslvr = linkResolver()
